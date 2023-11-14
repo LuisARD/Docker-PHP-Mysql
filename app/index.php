c36ec9396f4bfc103a8c04a27993fef736f2e883
LuisARD <luisangeldc08@gmail.com>
2023-11-14 15:17:15 -0400

    Primer commit

---
+++ app/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Compose</title>
+</head>
+<body>
+    <h1>Docker Compose</h1>
+
+    <?php
+    $nombre = "Luis Angel De La Cruz";
+    $matricula = "2021-1031";
+    echo "<p>Mi nombre es $nombre.</p>";
+    echo "<p>Mi matricula es $matricula.</p>";
+    ?>
+
+</body>
+</html>
